1c064d96f31305c0092f35521c41f9b23c79a12d
tanninaham <hamizi.tannina@gmail.com>
2024-10-21 23:44:14 +0200

    update

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.querySelector('.menu-button').addEventListener('click', function() {
-    document.querySelector('nav').classList.toggle('open');
+// script.js
+
+document.querySelectorAll('.menu > li').forEach(item => {
+    item.addEventListener('click', event => {
+        // Toggle pour afficher ou masquer le sous-menu
+        const submenu = item.querySelector('.submenu');
+        if (submenu) {
+            submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+        }
+    });
 });
